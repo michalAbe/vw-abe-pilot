a7f4c3e2c1897aa7d008fc8ccd2139272bbb9def
michalAbe <abebeats@gmail.com>
2025-08-10 15:39:18 +0200

    Update values.py
    chore(vw): add ENABLE_ABE_LANE_CHANGE flag

--- openpilot/selfdrive/car/volkswagen/values.py
+++ openpilot/selfdrive/car/volkswagen/values.py
@@ -1,4 +1,3 @@
-# Placeholder values for VW MEB platform (Skoda Enyaq)
-# TODO: define fingerprints/flags as needed for real integration
+# VW MEB flags (Abe pilot)
 class VWFlags:
-    ENABLE_ABE_LANE_CHANGE = True
+ENABLE_ABE_LANE_CHANGE = True
