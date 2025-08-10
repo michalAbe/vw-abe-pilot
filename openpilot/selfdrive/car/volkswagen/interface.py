f86b24dbf2e6bbc397bcbb726aed563424140202
michalAbe <abebeats@gmail.com>
2025-08-10 15:35:51 +0200

    Update interface.py
    feat(vw): hook to call abe_lane_change

--- openpilot/selfdrive/car/volkswagen/interface.py
+++ openpilot/selfdrive/car/volkswagen/interface.py
@@ -1,13 +1,23 @@
-# Placeholder Volkswagen interface hook for Abe lane change
-# TODO: import Abe lane change and call from VW interface update loop
+# VW interface hook – volanie Abe blinker LC logiky
 
 try:
-    from openpilot.selfdrive.controls.abe_lane_change import run_blinker_lane_change
+from openpilot.selfdrive.controls.abe_lane_change import run_blinker_lane_change
 except Exception:
-    def run_blinker_lane_change(*args, **kwargs):
-        pass
+def run_blinker_lane_change(*args, **kwargs):
+return None
+
+class PlannerStub:
+debug_note: str = ""
 
 class VWInterfacePlaceholder:
-    def update(self, car_state, model_data, planner):
-        # TODO: integrate with real VW interface
-        run_blinker_lane_change(car_state, model_data, planner)
+"""
+Placeholder trieda – v skutočnom openpilot VW interface sa toto
+zavolá z update loopu. Tu iba demonštrujeme, ako sa LC trigger volá.
+"""
+def __init__(self):
+self._planner = PlannerStub()
+
+def update(self, car_state, model_data):
+req = run_blinker_lane_change(car_state, model_data, self._planner)
+# TODO: ak req != None -> odovzdať požiadavku do skutočného planneru
+return req, self._planner.debug_note
